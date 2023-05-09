118d62413e690f31fb7d3ab841ebdd73e1ed9f37
Bala Sukesh <bbelide2@illinois.edu>
2023-05-09 17:16:41 -0500

    Update lda.py

--- backend/lda.py
+++ backend/lda.py
@@ -5,29 +5,27 @@ from nltk.corpus import stopwords
 from collections import Counter
 import gensim
 from gensim.corpora import Dictionary
-from gensim.models import LdaModel
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+from gensim.models.ldamodel import LdaModel
 
 def generateTopics(url):
-
-    print("lda: received request")
     # Retrieve webpage content
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    
     # Remove website-specific content such as navigation bars, footers, and advertisements
-    for element in soup(["script", "style", "nav", "footer", "aside", "iframe"]):
-        element.extract()
+    for script in soup(["script", "style"]):
+        script.decompose()
+    for div in soup.find_all('div', class_=lambda x: x != 'article'):
+        div.decompose()
+    for nav in soup.find_all('nav', class_=lambda x: x != 'article'):
+        nav.decompose()
     text = soup.get_text()
 
     # Preprocess text
     tokens = nltk.word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
     tokens = [t for t in tokens if not t in stop_words and t.isalpha()]
-
-    # Lemmatize tokens
-    lemmatizer = nltk.stem.WordNetLemmatizer()
+    lemmatizer = nltk.WordNetLemmatizer()
     tokens = [lemmatizer.lemmatize(t) for t in tokens]
 
     # POS tagging
@@ -36,35 +34,24 @@ def generateTopics(url):
     # Named entity recognition
     ner_tags = nltk.ne_chunk(pos_tags)
 
-    # Sentiment analysis
-    analyzer = SentimentIntensityAnalyzer()
-    sentiment_scores = analyzer.polarity_scores(text)
-    sentiment = 'neutral'
-    if sentiment_scores['compound'] > 0.1:
-        sentiment = 'positive'
-    elif sentiment_scores['compound'] < -0.1:
-        sentiment = 'negative'
-
     # Frequency analysis
     freq_dist = Counter(tokens)
 
     # LDA topic modeling
     dictionary = Dictionary([tokens])
     corpus = [dictionary.doc2bow([token]) for token in tokens]
-    lda_model = LdaModel(corpus, id2word=dictionary, num_topics=5)
-    topics = lda_model.show_topics(num_topics=5, num_words=10, formatted=False)
+    lda_model = LdaModel(corpus=corpus, id2word=dictionary, num_topics=5, passes=20, alpha='auto', eta='auto')
 
-    print("topics: ")
-    print(topics)
+    # Print topics
+    print("Topics:")
+    topics = lda_model.print_topics(num_topics=5, num_words=10)
+    for topic in topics:
+        print(topic)
 
-    # Refine topics based on sentiment
+    # Convert topics to a dictionary
     topic_dict = {}
     for topic in topics:
-        topic_dict[topic[0]] = [w[0] for w in topic[1]]
-    if sentiment == 'positive':
-        topic_dict = {k: v for k, v in topic_dict.items() if 'good' in v or 'great' in v or 'excellent' in v}
-    elif sentiment == 'negative':
-        topic_dict = {k: v for k, v in topic_dict.items() if 'bad' in v or 'poor' in v or 'terrible' in v}
+        topic_dict[topic[0]] = [w.split('*')[1].replace('"', '') for w in topic[1].split('+')]
 
     # Ranking topics by frequency
     topic_freq = {}
